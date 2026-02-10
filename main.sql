78549ed87d97217341ff1f69a12970f0b21f38ee
MCES10 <138902870+MCES10@users.noreply.github.com>
2026-02-10 10:06:24 +0000

    main

---
+++ main.sql
@@ -0,0 +1,14 @@
+/* Run Seperately in the Browser for SQLITE */
+
+INSERT INTO tblProduct (Name, Price)
+VALUES ("Chai Tea", 2.50)
+
+/* Run Seperately in the Browser for SQLITE */
+
+INSERT INTO tblProduct (Name, Price)
+VALUES ("Decaf Coffee", 1.50)
+
+/* Run Seperately in the Browser for SQLITE */
+
+INSERT INTO tblProduct (Name, Price)
+VALUES ("Hot Chocolate", 2.50)
